eb7ef5beccad98a823032b63a805a7e4e5ca82b5
ssrikar321321 <55238962+ssrikar321321@users.noreply.github.com>
2026-02-16 14:43:11 +0100

    Update enhanced_iv_analyzer.py

--- enhanced_iv_analyzer.py
+++ enhanced_iv_analyzer.py
@@ -46,10 +46,10 @@ st.title("⚡ Enhanced Discharge I-V Analysis")
 # Sidebar
 with st.sidebar:
     st.header("📁 Upload Files")
-    w1_v_file = st.file_uploader("Electrode 1 Voltage CSV", type=['csv'], key='w1v')
-    w1_i_file = st.file_uploader("Electrode 1 Current CSV", type=['csv'], key='w1i')
-    w2_v_file = st.file_uploader("Electrode 2 Voltage CSV", type=['csv'], key='w2v')
-    w2_i_file = st.file_uploader("Electrode 2 Current CSV", type=['csv'], key='w2i')
+    w1_v_file = st.file_uploader("Electrode 1 Voltage file", type=['csv'], key='w1v')
+    w1_i_file = st.file_uploader("Electrode 1 Current file", type=['csv'], key='w1i')
+    w2_v_file = st.file_uploader("Electrode 2 Voltage file", type=['csv'], key='w2v')
+    w2_i_file = st.file_uploader("Electrode 2 Current file", type=['csv'], key='w2i')
     
     st.divider()
     st.header("⚙️ Settings")
